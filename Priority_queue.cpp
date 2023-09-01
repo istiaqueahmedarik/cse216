d5ea42a7ff9577708d8037e854461656f8869bce
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2023-09-01 21:03:29 +0600

    heap cpp

--- Priority_queue.cpp
+++ Priority_queue.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
-#include <limits>
-#include <algorithm>
+#include <bits/stdc++.h>
 template <class T>
 class Heap
 {
+protected:
     T *heap;
-    size_t heapSize, lastLeafIndex;
+    unsigned long long heapSize, lastLeafIndex;
     bool IsMaxHeap;
 
     void heapSizeMaintainer()
@@ -14,35 +13,35 @@ class Heap
             return;
         T *temp_heap = new T[2 * heapSize];
         heapSize *= 2;
-        for (size_t i = 0; i <= lastLeafIndex; i++)
+        for (unsigned long long i = 0; i <= lastLeafIndex; i++)
             temp_heap[i] = heap[i];
         delete[] heap;
         heap = temp_heap;
     }
-    static inline size_t parent(size_t i) { return i == 1 ? 1 : i / 2; }
-    static inline size_t left(size_t i) { return i * 2; }
-    static inline size_t right(size_t i) { return i * 2 + 1; }
+    static inline unsigned long long parent(unsigned long long i) { return i == 1 ? 1 : i / 2; }
+    static inline unsigned long long left(unsigned long long i) { return i * 2; }
+    static inline unsigned long long right(unsigned long long i) { return i * 2 + 1; }
 
-    bool isPresent(size_t i) { return i <= lastLeafIndex; }
+    bool isPresent(unsigned long long i) { return i <= lastLeafIndex; }
 
     T maxLimit() { return std::numeric_limits<T>::max(); }
     T minLimit() { return std::numeric_limits<T>::min(); }
 
-    bool HeapProperty(size_t i) { return IsMaxHeap ? heap[parent(i)] >= heap[i] : heap[parent(i)] <= heap[i]; }
+    bool HeapProperty(unsigned long long i) { return IsMaxHeap ? heap[parent(i)] >= heap[i] : heap[parent(i)] <= heap[i]; }
 
-    T getMinAmongParentAndChildren(size_t i)
+    T getMinAmongParentAndChildren(unsigned long long i)
     {
         bool leftPresence = isPresent(left(i)), rightPresence = isPresent(right(i));
         return std::min({heap[i], leftPresence ? heap[left(i)] : maxLimit(), rightPresence ? heap[right(i)] : maxLimit()});
     }
 
-    T getMaxAmongParentAndChildren(size_t i)
+    T getMaxAmongParentAndChildren(unsigned long long i)
     {
         bool leftPresence = isPresent(left(i)), rightPresence = isPresent(right(i));
         return std::max({heap[i], leftPresence ? heap[left(i)] : minLimit(), rightPresence ? heap[right(i)] : minLimit()});
     }
 
-    void down_hepify(size_t i)
+    void down_hepify(unsigned long long i)
     {
         if (!isPresent(i))
             return;
@@ -58,7 +57,7 @@ class Heap
         else if (rightPresence and heap[right(i)] == factor)
             std::swap(heap[i], heap[right(i)]), down_hepify(right(i));
     }
-    void up_heapify(size_t i)
+    void up_heapify(unsigned long long i)
     {
         if (not isPresent(i))
             return;
@@ -68,7 +67,7 @@ class Heap
 
 public:
     // true means max heap and false means min heap
-    Heap(size_t size = 2, bool IsMaxHeap = true) : heapSize(size), lastLeafIndex(0)
+    Heap(unsigned long long size = 2, bool IsMaxHeap = true) : heapSize(size), lastLeafIndex(0)
     {
         this->IsMaxHeap = IsMaxHeap;
         this->heap = new T[size];
@@ -82,7 +81,7 @@ public:
     {
         this->IsMaxHeap = h.IsMaxHeap;
         this->heap = new T[heapSize];
-        for (size_t i = 0; i <= lastLeafIndex; i++)
+        for (unsigned long long i = 0; i <= lastLeafIndex; i++)
         {
             this->heap[i] = h.heap[i];
         }
@@ -104,13 +103,13 @@ public:
     }
     void build_heap()
     {
-        for (size_t i = lastLeafIndex / 2; i > 0; i--)
+        for (unsigned long long i = lastLeafIndex / 2; i > 0; i--)
             down_hepify(i);
     }
 
     void pop() { erase(1); }
 
-    void erase(size_t idx)
+    void erase(unsigned long long idx)
     {
         if (idx > lastLeafIndex)
         {
@@ -126,11 +125,11 @@ public:
     }
     void print(std::ostream &os = std::cout)
     {
-        for (size_t i = 1; i <= lastLeafIndex; i++)
+        for (unsigned long long i = 1; i <= lastLeafIndex; i++)
             os << heap[i] << " ";
         os << std::endl;
     }
-    size_t size() { return lastLeafIndex; }
+    unsigned long long size() { return lastLeafIndex; }
     bool empty() { return lastLeafIndex == 0; }
     bool is_max_heap() { return IsMaxHeap; }
     bool is_min_heap() { return !IsMaxHeap; }
@@ -181,6 +180,10 @@ public:
         pop();
         return ret;
     }
+};
+template <class T>
+class Priority_queue : Heap<T>
+{
 };
 int main()
 {
